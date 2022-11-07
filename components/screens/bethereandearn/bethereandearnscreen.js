0fcd4f7d5186e7d998602ada374df986296d9962
Max <wenglokthong@gmail.com>
2022-11-08 00:49:56 +0800

    exp circle

--- components/screens/bethereandearn/bethereandearnscreen.js
+++ components/screens/bethereandearn/bethereandearnscreen.js
@@ -3,16 +3,21 @@ import {
   worldxstyleconstants,
   worldxstyles,
 } from "../../../stylesheets/worldxstylesheet";
+import { Emitter } from "react-native-particles";
 import { useState, useEffect, createRef } from "react";
-import MapView from "react-native-maps";
+import MapView, { Circle } from "react-native-maps";
 import * as Location from "expo-location";
+import { isPointWithinRadius } from "geolib";
 
 export const BeThereAndEarnScreen = () => {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   const [geocode, setGeocode] = useState(null);
-  const mapRef = createRef();
+  const [expCirclesArray, setExpCirclesArray] = useState(null);
+  const [expCirclesDataArray, setExpCirclesDataArray] = useState(null);
+  const [isInExpCircle, setIsInExpCircle] = useState(null);
 
+  const mapRef = createRef();
   const goToMyLocation = async (_latitude, _longitude) => {
     mapRef.current.animateCamera({
       center: {
@@ -22,6 +27,8 @@ export const BeThereAndEarnScreen = () => {
       zoom: 20,
     });
   };
+
+  //ON MOUNT
   useEffect(() => {
     var interval;
     (async () => {
@@ -31,6 +38,49 @@ export const BeThereAndEarnScreen = () => {
         return;
       }
 
+      //FIRST TIME SET POS
+      let location = await Location.getCurrentPositionAsync({
+        timeInterval: 5000,
+      });
+
+      let geocode = await Location.reverseGeocodeAsync(location.coords);
+      setLocation(location);
+      setGeocode(geocode);
+
+      const _expCircleDataArray = GenerateExpCirclesData({
+        userlatlong: {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        },
+        numCircles: 5,
+        minRadius: 5,
+        maxRadius: 10,
+        minDistance: -0.00025,
+        maxDistance: 0.00025,
+      });
+
+      //generate exp circles data
+      setExpCirclesDataArray(_expCircleDataArray);
+
+      setExpCirclesArray(() => {
+        return _expCircleDataArray.map((element, index) => {
+          return (
+            <Circle
+              key={index}
+              center={{
+                latitude: element.latitude,
+                longitude: element.longitude,
+              }}
+              radius={element.radius}
+              strokeWidth={2}
+              strokeColor="#c681f5"
+              fillColor="rgba(74,29,119,0.5)"
+            />
+          );
+        });
+      });
+
+      //fetch location updates every interval
       interval = setInterval(async () => {
         let location = await Location.getCurrentPositionAsync({
           timeInterval: 5000,
@@ -39,23 +89,35 @@ export const BeThereAndEarnScreen = () => {
         let geocode = await Location.reverseGeocodeAsync(location.coords);
         setLocation(location);
         setGeocode(geocode);
-      }, 2000);
+      }, 1000);
     })();
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); //impt! cleanup on unmount
   }, []);
 
+  //eveytime the location change
   useEffect(() => {
     if (location == null) return;
 
+    //animate map to go to point
     goToMyLocation(location.coords.latitude, location.coords.longitude);
-  }, [location]);
 
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+    //check if user is in exp circle
+    var isUserInAnyCircle = false;
+    expCirclesDataArray?.forEach((element) => {
+      isUserInAnyCircle |= isPointWithinRadius(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+        },
+        {
+          latitude: element.latitude,
+          longitude: element.longitude,
+        },
+        element.radius
+      );
+    });
+    setIsInExpCircle(isUserInAnyCircle);
+  }, [location]);
 
   return (
     <View style={{ flex: 1, justifyContent: "center" }}>
@@ -72,33 +134,103 @@ export const BeThereAndEarnScreen = () => {
         ></Image>
       </View>
       <View style={[styles.container, { flex: 5 }]}>
-        <MapView
-          ref={mapRef}
-          style={[styles.mapContainer]}
-          customMapStyle={styles.customMapStyle}
-          followsUserLocation={true}
-          showsUserLocation={true}
-          zoomEnabled={true}
-        />
-      </View>
-      <View
-        style={[
-          styles.container,
-          styles.container,
-          worldxstyles.bordered,
-
-          { flex: 1 },
-        ]}
-      >
-        {geocode != null && (
-          <Text
-            style={worldxstyles.text}
-          >{`${geocode[0].city} , ${geocode[0].country}, ${geocode[0].district}, ${geocode[0].isoCountryCode}, ${geocode[0].name}, ${geocode[0].postalCode}, ${geocode[0].region}, ${geocode[0].street}, ${geocode[0].streetNumber}, ${geocode[0].subregion}, ${geocode[0].timezone}`}</Text>
-        )}
+        <View
+          style={[
+            styles.mapContainer,
+            styles.container,
+            worldxstyles.bordered,
+            { flex: 5, overflow: "hidden" },
+          ]}
+        >
+          <MapView
+            ref={mapRef}
+            style={[styles.mapContainer]}
+            customMapStyle={styles.customMapStyle}
+            followsUserLocation={true}
+            showsUserLocation={true}
+            zoomEnabled={true}
+          >
+            {expCirclesArray}
+          </MapView>
+        </View>
+        <View
+          style={[
+            styles.container,
+            styles.container,
+            worldxstyles.bordered,
+            { width: "80%" },
+          ]}
+        >
+          {/* {geocode != null ? (
+            <Text
+              style={[
+                worldxstyles.text,
+                {
+                  textAlign: "center",
+                  position: "absolute",
+                  top: 0,
+                  marginTop: 10,
+                },
+              ]}
+            >{`${geocode[0].city} , ${geocode[0].country}, ${geocode[0].district}, ${geocode[0].isoCountryCode}, ${geocode[0].name}, ${geocode[0].postalCode}, ${geocode[0].region}, ${geocode[0].street}, ${geocode[0].streetNumber}, ${geocode[0].subregion}, ${geocode[0].timezone}`}</Text>
+          ) : (
+            <Text style={worldxstyles.text}>Waiting...</Text>
+          )} */}
+          {isInExpCircle ? (
+            <Text style={worldxstyles.text}>You are in a reward circle!</Text>
+          ) : (
+            <Emitter
+              numberOfParticles={50}
+              emissionRate={1}
+              interval={0}
+              particleLife={1500}
+              direction={-90}
+              spread={45}
+              speed={10}
+              autoStart={true}
+              infiniteLoop={true}
+              fromPosition={{ x: 200, y: 0 }}
+            >
+              <Text style={worldxstyles.text}>Particle</Text>
+            </Emitter>
+          )}
+        </View>
       </View>
     </View>
   );
 };
+
+//HELPER
+const GenerateExpCirclesData = (props) => {
+  var circleDataArray = [];
+  const userlatlong = props.userlatlong;
+  const minRadius = props.minRadius;
+  const maxRadius = props.maxRadius;
+
+  for (let i = 0; i < props.numCircles; ++i) {
+    const radius =
+      Math.floor(Math.random() * (maxRadius - minRadius)) + minRadius;
+    const spawnlatlong = {
+      latitude:
+        userlatlong.latitude +
+        Math.random() * (props.maxDistance - props.minDistance) +
+        props.minDistance,
+      longitude:
+        userlatlong.longitude +
+        Math.random() * (props.maxDistance - props.minDistance) +
+        props.minDistance,
+    };
+
+    circleDataArray.push({
+      latitude: spawnlatlong.latitude,
+      longitude: spawnlatlong.longitude,
+      radius: radius,
+    });
+  }
+
+  return circleDataArray.map((element) => element);
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
